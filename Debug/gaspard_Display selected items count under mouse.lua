9125bd45e395466b0c0dc7833d4657dd13bba16e
Gaspard Broussaud <gaspard@g4f.fr>
2024-02-28 11:38:59 +0100

    New selected items count display script

---
+++ Debug/gaspard_Display selected items count under mouse.lua
@@ -0,0 +1,21 @@
+--@description Display selected items count under mouse
+--@author gaspard
+--@version 1.0
+--@changelog
+--    Initial release
+--@about
+--    Display selected items count under mouse cursor.
+
+item_count = reaper.CountSelectedMediaItems(0)
+text = "Number of selected items: "..tostring(item_count)
+
+function Show_Tooltip(text)
+    local x, y = reaper.GetMousePosition()
+    reaper.TrackCtl_SetToolTip(text:upper():gsub('.','%0 '), x, y, true) -- spaced out // topmost true
+end
+
+if item_count == 0 then
+    Show_Tooltip("\n\n no selected items \n\n")
+else
+    Show_Tooltip("\n\n "..text.."\n\n")
+end
