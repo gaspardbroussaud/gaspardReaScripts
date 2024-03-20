8b6d216642ee18a46a614aff4c7f13ffe79a874a
Gaspard Broussaud <gaspard@g4f.fr>
2024-03-20 11:34:23 +0100

    Update script

--- Track/gaspard_Mute track and hide in TCP and MCP.lua
+++ Track/gaspard_Mute track and hide in TCP and MCP.lua
@@ -1,17 +1,35 @@
 --@description Mute track and hide in TCP and MCP
 --@author gaspard
---@version 1.1
---@changelog +Locking tracks when hiding.
+--@version 1.2
+--@changelog +Togglable state.
 --@about Mute and hide in TCP and MCP all selected tracks. Hide children from TCP and MCP if selected track is parent.
 
 -- HIDE and MUTE TRACK --
 function hideMute(track, mute)
-    reaper.SetMediaTrackInfo_Value(track, "B_MUTE", mute)
     if mute == 1 then
+        reaper.SetMediaTrackInfo_Value(track, "B_MUTE", mute)
         reaper.Main_OnCommand(41312, 0) -- Lock selected track
     end
-    reaper.SetMediaTrackInfo_Value(track, "B_SHOWINMIXER", 0)
-    reaper.SetMediaTrackInfo_Value(track, "B_SHOWINTCP", 0)
+    
+    if mute == 8 then
+        reaper.SetMediaTrackInfo_Value(track, "B_SHOWINMIXER", 0)
+        reaper.SetMediaTrackInfo_Value(track, "B_SHOWINTCP", 0)
+    else
+        viewState = math.abs(mute - 1)
+        reaper.SetMediaTrackInfo_Value(track, "B_SHOWINMIXER", viewState)
+        reaper.SetMediaTrackInfo_Value(track, "B_SHOWINTCP", viewState)
+    end
+    
+    if mute == 0 then
+        reaper.Main_OnCommand(41313, 0) -- Unlock selected track
+        reaper.SetMediaTrackInfo_Value(track, "B_MUTE", mute)
+    end
+end
+
+-- CHECK TRACK STATE --
+function trackState(track)
+    mute = reaper.GetMediaTrackInfo_Value(track, "B_SHOWINTCP")
+    hideMute(track, mute)
 end
 
 -- MAIN FUNCTION --
@@ -28,25 +46,25 @@ function main()
         if reaper.GetMediaTrackInfo_Value(trackTab[i], "I_FOLDERDEPTH") == 1 then
             reaper.SetTrackSelected(trackTab[i], true)
             
-            hideMute(trackTab[i], 1)
+            trackState(trackTab[i])
             
-            -- Select only children of selected folders --
-            reaper.Main_OnCommand(reaper.NamedCommandLookup("_SWS_SELCHILDREN"), 0)
-            
-            reaper.SetTrackSelected(trackTab[i], false)
-            
-            local children_count = reaper.CountSelectedTracks(0)
-            for i = 0, children_count - 1 do
-                local child_track = reaper.GetSelectedTrack(0, i)
-                hideMute(child_track, 0)
+            if mute == 1 then
+                -- Select only children of selected folders --
+                reaper.Main_OnCommand(reaper.NamedCommandLookup("_SWS_SELCHILDREN"), 0)
+                
+                reaper.SetTrackSelected(trackTab[i], false)
+                
+                local children_count = reaper.CountSelectedTracks(0)
+                for i = 0, children_count - 1 do
+                    local child_track = reaper.GetSelectedTrack(0, i)
+                    hideMute(child_track, 8)
+                end
+                
+                reaper.Main_OnCommand(40297, 0) -- Clear selection of all tracks
             end
-            
-            reaper.Main_OnCommand(40297, 0) -- Clear selection of all tracks
-            --hideMute(trackTab[i], 1)
-            
         else
             reaper.SetTrackSelected(trackTab[i], true)
-            hideMute(trackTab[i], 1)
+            trackState(trackTab[i])
             reaper.SetTrackSelected(trackTab[i], false)
         end
     end
