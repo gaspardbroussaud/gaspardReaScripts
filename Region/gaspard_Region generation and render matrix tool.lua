a41c9369d38c59301fb03411a9080e87fab79f80
Gaspard Broussaud <gaspard@g4f.fr>
2024-10-30 17:29:19 +0100

    Region generation and render matrix Tool v1.0.4

--- Region/gaspard_Region generation and render matrix tool.lua
+++ Region/gaspard_Region generation and render matrix tool.lua
@@ -1,8 +1,8 @@
 -- @description Region generation and render matrix Tool
 -- @author gaspard
--- @version 1.0.3
+-- @version 1.0.4
 -- @changelog
---  • Added region colors based on their corresponding render track colors.
+--  • Fix "_010" numbering if 10th cluster is last cluster in folder.
 -- @about
 --  Retrieves all selected items, identifies clusters where the selected tracks serve as parents, and uses these clusters as the region's name and render matrix.
 --  To use: select items to detect clusters and the tracks through which to render.
@@ -11,15 +11,21 @@
 local color_regions = true
 ---------------------------
 
+----------------------------------------------------------------
+-- INIT VARIABLES
+local render_tracks = {}
+local render_tracks_name = {}
+local render_folders = {}
+
 -- UTILITY FUNCTIONS
 -- MESSAGE BOX ON ERROR
 ---@param message string
-function Utility_MessageBox(message)
+function MessageBox(message)
     reaper.MB(tostring(message), "Message box", 0)
 end
 
 -- SORT VALUES FUNCTION
-function Utility_SortOnValue(t,...)
+function SortOnValue(t,...)
     local a = {...}
     table.sort(t, function (u,v)
         for i in pairs(a) do
@@ -34,7 +40,7 @@ end
 ---@param track any
 ---@param target any
 ---@return boolean is_in_target
-function Utility_GetParentTrackMatch(track, target)
+function GetParentTrackMatch(track, target)
     while true do
         local parent = reaper.GetParentTrack(track)
         if parent then
@@ -51,8 +57,8 @@ end
 
 -- SYSTEM FUNCTIONS
 -- GET SELECTED TRACKS TAB
-function System_GetSelectedTracksTab()
-    track_count = reaper.CountSelectedTracks(0)
+function GetSelectedTracksTab()
+    local track_count = reaper.CountSelectedTracks(0)
     render_tracks = {}
     render_tracks_name = {}
     render_folders = {}
@@ -70,52 +76,51 @@ function System_GetSelectedTracksTab()
             table.insert(render_tracks_name, track_name)
         end
     else
-        Utility_MessageBox("Please select at least one track that contains selected items.")
+        MessageBox("Please select at least one track that contains selected items.")
     end
 end
 
 -- GET ALL ITEMS FROM PROJECT AND ASIGN TO PARENT TABLE
-function System_GetItemsFromProject()
-    item_count = reaper.CountSelectedMediaItems(0)
+function GetItemsFromProject()
+    local item_count = reaper.CountSelectedMediaItems(0)
     if item_count ~= 0 then
         -- Get all selected tracks
-        System_GetSelectedTracksTab()
+        GetSelectedTracksTab()
 
         for i = 0, item_count - 1 do
             local cur_item = reaper.GetSelectedMediaItem(0, i)
             local cur_start = reaper.GetMediaItemInfo_Value(cur_item, "D_POSITION")
             local cur_track = reaper.GetMediaItemTrack(cur_item)
-            --local cur_track_id = reaper.GetMediaTrackInfo_Value(cur_track, "IP_TRACKNUMBER")
 
             for j = 1, #render_tracks do
-                if cur_track == render_tracks[j] or Utility_GetParentTrackMatch(cur_track, render_tracks[j]) then
+                if cur_track == render_tracks[j] or GetParentTrackMatch(cur_track, render_tracks[j]) then
                     table.insert(render_folders[j], { item = cur_item, start = cur_start })
                 end
             end
         end
 
         -- Sort items in parent tables
-        System_SortItemsInTimelineOrder()
+        SortItemsInTimelineOrder()
     else
-        Utility_MessageBox("Please select at least one item along with its corresponding track and/or parent track.")
+        MessageBox("Please select at least one item along with its corresponding track and/or parent track.")
     end
 end
 
 -- SORT ITEMS BY FOLDERS IN TIMELINE ORDER
-function System_SortItemsInTimelineOrder()
+function SortItemsInTimelineOrder()
     for i = 1, #render_folders do
-        Utility_SortOnValue(render_folders[i], "start")
+        SortOnValue(render_folders[i], "start")
     end
 
     for i = 1, #render_folders do
         if #render_folders[i] ~= 0 then
-            System_FindClusters(render_folders[i], render_tracks_name[i], render_tracks[i])
+            FindClusters(render_folders[i], render_tracks_name[i], render_tracks[i])
         end
     end
 end
 
 -- FIND CLUSTER BY FOLDERS
-function System_FindClusters(folder, region_name, render_track)
+function FindClusters(folder, region_name, render_track)
     local first_start = folder[1].start
     local prev_end = first_start + reaper.GetMediaItemInfo_Value(folder[1].item, "D_LENGTH")
     local last_end = prev_end
@@ -125,7 +130,7 @@ function System_FindClusters(folder, region_name, render_track)
         local cur_start = reaper.GetMediaItemInfo_Value(folder[i].item, "D_POSITION")
         local cur_end = cur_start + reaper.GetMediaItemInfo_Value(folder[i].item, "D_LENGTH")
         local suffix = "_"
-        if index < 10 then suffix = "_0" end
+        if index < 9 then suffix = "_0" end
 
         if prev_end + 0.000001 < cur_start then
             index = index + 1
@@ -147,6 +152,7 @@ function System_FindClusters(folder, region_name, render_track)
             local display = region_name
             if index ~= 0 then
                 index = index + 1
+                if index > 9 then suffix = "_" end
                 display = region_name..suffix..tostring(index)
             end
 
@@ -166,7 +172,7 @@ end
 reaper.Undo_BeginBlock()
 reaper.PreventUIRefresh(-1)
 
-System_GetItemsFromProject()
+GetItemsFromProject()
 
 reaper.PreventUIRefresh(1)
 reaper.Undo_EndBlock("Create region for clusters of selected items by selected tracks", -1)
