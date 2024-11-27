ff08df578c45ec726340e2f7004de9f437c08a5a
Gaspard Broussaud <gaspard@g4f.fr>
2024-11-27 18:05:26 +0100

    Probably last try

--- GUI/GUI_Style_Settings.lua
+++ GUI/GUI_Style_Settings.lua
@@ -2,7 +2,7 @@
 --@description Gui Settings for all gaspard's scripts
 --@author gaspard
 --@version 1.0.0
---@provides [nomain] Gui_Style_Settings.lua
+--@provides [main=Scripts/Gaspard ReaScripts/GUI] Gui_Style_Settings.lua
 --@about Gui Settings for all gaspard's scripts, called at frame 1 on each GUI script.
 
 local style_vars = {
