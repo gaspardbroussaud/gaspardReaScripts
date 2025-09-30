2c34118e25c1e33826f92fa59efc365e6aadd8d6
Gaspard <gaspard@g4f.fr>
2025-09-30 11:55:44 +0200

    GRL v1.0.4

--- General/gaspard_GaspaReaLauncher.lua
+++ General/gaspard_GaspaReaLauncher.lua
@@ -1,8 +1,8 @@
 --@description GaspaReaLauncher
 --@author gaspard
---@version 1.0.3
+--@version 1.0.4
 --@changelog
---  - Update police path
+--  - Fix font loading
 --@about
 --  # Gaspard Reaper Launcher
 --  Reaper Launcher for projects.
@@ -113,8 +113,8 @@ local font_size = 16
 
 -- ImGui Init
 local ctx = reaper.ImGui_CreateContext('gaspard_rea_launcher_ctx')
-local arial_font = reaper.ImGui_CreateFont(GUI_STYLE.FONTS.ARIAL.CLASSIC)
-local italic_arial_font = reaper.ImGui_CreateFont(GUI_STYLE.FONTS.ARIAL.CLASSIC, reaper.ImGui_FontFlags_Italic())
+local arial_font = reaper.ImGui_CreateFontFromFile(GUI_STYLE.FONTS.ARIAL.CLASSIC)
+local italic_arial_font = reaper.ImGui_CreateFontFromFile(GUI_STYLE.FONTS.ARIAL.CLASSIC, reaper.ImGui_FontFlags_Italic())
 local icon_font = reaper.ImGui_CreateFontFromFile(GUI_STYLE.FONTS.ICONS)
 reaper.ImGui_Attach(ctx, arial_font)
 reaper.ImGui_Attach(ctx, italic_arial_font)
