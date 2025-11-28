b4f00879df3ffa5a5f5ccde55db54b5d35461a4c
Gaspard <gaspard@g4f.fr>
2025-11-28 10:35:12 +0100

    SSFTS v1.0.1

--- TCP/gaspard_Solo state follows track selection.lua
+++ TCP/gaspard_Solo state follows track selection.lua
@@ -1,25 +1,30 @@
 --@description Solo state follows track selection
 --@author gaspard
---@version 1.0.0
---@changelog Init
+--@version 1.0.1
+--@changelog Restore solo states when exiting
 --@about Solo state follows track selection
+
+local restore_list = {}
+local solo_type = 2 -- Solo in place == 2 ; Solo (ignore routing) == 1
 
 -- Restoring solos
 local function RestoreSolo()
-    reaper.ShowConsoleMsg("\nEND")
     reaper.Main_OnCommand(40340, 0) -- Unsolo all tracks
+    for i, element in ipairs(restore_list) do
+        local track = reaper.BR_GetMediaTrackByGUID(0, element.GUID)
+        if track then
+            reaper.SetMediaTrackInfo_Value(track, "I_SOLO", element.solo_type)
+        end
+    end
 end
 
 -- Toggle button state in Reaper
 local function SetButtonState(set)
-    if set ~= 1 then
-        RestoreSolo()
-    end
+    if set ~= 1 then RestoreSolo() end
     local _, _, sec, cmd, _, _, _ = reaper.get_action_context()
     reaper.SetToggleCommandState(sec, cmd, set or 0)
     reaper.RefreshToolbar2(sec, cmd)
 end
-SetButtonState(1)
 
 -- Main loop
 local function Loop()
@@ -28,11 +33,11 @@ local function Loop()
     reaper.PreventUIRefresh(1)
     for i = 1, track_count do
         local track = reaper.GetTrack(0, i - 1)
-        local solo = reaper.GetMediaTrackInfo_Value(track, "I_SOLO") == 1
+        local solo = reaper.GetMediaTrackInfo_Value(track, "I_SOLO") == solo_type
 
         if reaper.IsTrackSelected(track) then
             if solo == false then
-                reaper.SetMediaTrackInfo_Value(track, "I_SOLO", 1)
+                reaper.SetMediaTrackInfo_Value(track, "I_SOLO", solo_type)
             end
         else
             if solo == true then
@@ -44,6 +49,18 @@ local function Loop()
     reaper.UpdateArrange()
 
     reaper.defer(Loop)
+end
+
+-- Execution
+SetButtonState(1)
+
+local track_count = reaper.CountTracks(0)
+for i = 1, track_count do
+    local track = reaper.GetTrack(0, i - 1)
+    local solo = reaper.GetMediaTrackInfo_Value(track, "I_SOLO")
+    if solo > 0 then
+        restore_list[#restore_list+1] = {GUID = reaper.GetTrackGUID(track), solo_type = solo}
+    end
 end
 
 reaper.defer(Loop)
