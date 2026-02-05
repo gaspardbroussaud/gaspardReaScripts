52668ea53e63a0eeb2055927cdc5863b440ae024
Gaspard <gaspard@g4f.fr>
2026-02-05 12:48:02 +0100

    TSOTUMCE v1.0.2

--- Track/gaspard_Toggle solo on track under mouse cursor exclusive.lua
+++ Track/gaspard_Toggle solo on track under mouse cursor exclusive.lua
@@ -1,23 +1,24 @@
 --@description Toggle solo on track under mouse cursor exclusive
 --@author gaspard
---@version 1.0.1
---@changelog UI Refresh upgrade
+--@version 1.0.2
+--@changelog Fix unsolo
 --@about Toggle solo on track under mouse cursor exclusive.
 
 local x, y = reaper.GetMousePosition()
 local track, info = reaper.GetTrackFromPoint(x, y)
 if not track or info ~= 0 then return end
 
+if reaper.CountSelectedTracks(0) == 1 and track == reaper.GetSelectedTrack(0, 0) then
+    reaper.SetMediaTrackInfo_Value(track, "I_SOLO", 0)
+    return
+end
+
 reaper.PreventUIRefresh(1)
 reaper.Undo_BeginBlock()
 
 reaper.Main_OnCommand(40340, 0) -- Track: Unsolo all tracks
 
-if reaper.GetMediaTrackInfo_Value(track, "I_SOLO") == 0 then
-    reaper.SetMediaTrackInfo_Value(track, "I_SOLO", 2)
-else
-    reaper.SetMediaTrackInfo_Value(track, "I_SOLO", 0)
-end
+reaper.SetMediaTrackInfo_Value(track, "I_SOLO", 2)
 
 reaper.Undo_EndBlock("Toggle solo on track under mouse cursor exclusive", -1)
 reaper.PreventUIRefresh(-1)
