5ea9dfbee3dd945b3ddba5f299fc1eb7d70cff53
Gaspard <gaspard@g4f.fr>
2026-07-28 16:07:29 +0200

    IROSI v1.0.0 x3

---
+++ Item/gaspard_Insert Raum on selected items.lua
@@ -0,0 +1,26 @@
+--@description Insert Raum on selected items
+--@author gaspard
+--@version 1.0.0
+--@changelog Init
+--@about Insert Raum on selected items.
+
+local sel_item_count = reaper.CountSelectedMediaItems(0)
+
+if sel_item_count < 1 then return end
+
+reaper.Undo_BeginBlock()
+reaper.PreventUIRefresh(1)
+
+local sel_items = {}
+
+for i = 1, sel_item_count do
+    sel_items[i] = reaper.GetSelectedMediaItem(0, i - 1)
+end
+
+for _, item in ipairs(sel_items) do
+    reaper.TakeFX_AddByName(reaper.GetMediaItemTake(item, 0), "Raum", -1)
+end
+
+reaper.PreventUIRefresh(-1)
+reaper.Undo_EndBlock("Add Raum to selected items", -1)
+reaper.UpdateArrange()
